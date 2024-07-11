7e74a5fb2ecb056616fb128126ac8938bcd1fb4a
poojasrisekar <poojoo.shree@gmail.com>
2024-07-11 21:38:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import AdminDashboard from "./Components/AdminDashboard";
+import "./App.css"; // Import your CSS file
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <AdminDashboard />
+    </div>
+  );
+};
+
+export default App;
